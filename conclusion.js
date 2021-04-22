ce20eb4076204f1a2228353b4a78f74462672b9a
NaoGL <82893990+NaoGL@users.noreply.github.com>
2021-04-22 00:27:15 -0500

    Add files via upload

---
+++ conclusion.js
@@ -0,0 +1,8 @@
+function mensaje(img)
+{
+	if (img == 1) 
+	{
+		sal = "Gracias por visitar mi pagina. Esta actividad me ha parecido muy entretenida de hacer y agregue algunos elementos aprendidos a lo largo del curso, me esforce mucho por ello y estoy satisfecha con el resultado pero ansio aprender más."
+		return sal;
+	}
+}
